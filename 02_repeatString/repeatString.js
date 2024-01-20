fa29f6d084d87e545323d8c3eb189faab490bd41
Hyalskavran <gorniakadrian94@gmail.com>
2024-01-20 22:08:18 +0100

    01 and 02 exercises made and completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (parseInt(times) < 0) {
+        return "ERROR"
+    } else {
+        let repeatedWord = []
 
+    for (let i = 0; i < parseInt(times); i++) {
+        repeatedWord.push(word)
+    }
+
+    return repeatedWord.join("")
+    }
 };
 
 // Do not edit below this line
