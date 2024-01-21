cf0d561e2cc4c2ff69f9288ae19e986203591b08
Hyalskavran <gorniakadrian94@gmail.com>
2024-01-21 22:49:29 +0100

    04 exercise done with the help of chatgpt

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    // Convert single value to an array
+    if (!Array.isArray(valuesToRemove)) {
+        valuesToRemove = [valuesToRemove];
+    }
 
+    // Create a new array by filtering out the values to be removed
+    let newArray = arr.filter(item => !valuesToRemove.includes(item));
+
+    return newArray;
 };
 
 // Do not edit below this line
