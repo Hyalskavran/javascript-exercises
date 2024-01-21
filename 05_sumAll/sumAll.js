0c0d353c19157e54d40280b2aa9698e0193f4562
Hyalskavran <gorniakadrian94@gmail.com>
2024-01-21 23:03:52 +0100

    exercise done and completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a == "number" && typeof b == "number") {
+        if (a < 0 || b < 0) {
+            return "ERROR"
+        }
+        else if (a < b) {
+            let sumAB = 0
+            for (a ; a <= b ; a++) {
+                sumAB += a
+            }
+            return sumAB
+        }
+        else if (a > b) {
+            let sumBA = 0
+            for (b ; b <= a ; b++) {
+                sumBA += b
+            }
+            return sumBA
+        }
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
