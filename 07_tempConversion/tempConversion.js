1f510c2187c1ce6cbac5549aaf4b8bac5d9fc6db
Hyalskavran <gorniakadrian94@gmail.com>
2024-01-22 17:41:38 +0100

    exercise done and completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  calcTemp = (temp - 32) * 5/9
+  if (Number.isInteger(calcTemp)) {
+    return calcTemp
+  } else {
+    return Math.round(calcTemp * 10) / 10
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  calcTemp = (temp * 9/5) + 32
+  if (Number.isInteger(calcTemp)) {
+    return calcTemp
+  } else {
+    return Math.round(calcTemp * 10) / 10
+  }
 };
 
 // Do not edit below this line
