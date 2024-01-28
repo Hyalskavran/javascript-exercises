911000cb1cc9aae7c6feb5bbf916d9b00f2fd6f7
Hyalskavran <gorniakadrian94@gmail.com>
2024-01-28 21:23:18 +0100

    exercise done with a little help of ChatGPT for redux in JS

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (args) {
+    // return [...args].filter(arg => args.charCodeAt(args.indexOf(arg)) > 64 )
 
+    let arrToCheck = [...args]
+        .filter(arg => /^[a-zA-Z0-9]+$/.test(arg))
+        .map(arg => arg.toLowerCase())
+
+    let arrReversed = arrToCheck.toReversed()
+
+    for (let i = 0 ; i < arrToCheck.length ; i++) {
+        if (arrToCheck[i] !== arrReversed[i]) {
+            return false
+        }
+    }
+
+    return true
 };
 
 // Do not edit below this line
