bf2d07760afb50d1ac81de1405a61e051bd9097c
Hyalskavran <gorniakadrian94@gmail.com>
2024-01-28 21:40:15 +0100

    exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num == 0) {
+        return 0
+    } else if (num < 0) {
+        return "OOPS"
+    } else {
+        let seq = []
 
+        for (let i = 0 ; i < num ; i++) {
+            if (i == 0 || i == 1) {
+                seq.push(1)
+            } else {
+                seq.push(seq[i - 1] + seq[i - 2])
+            }
+        }
+
+        return seq[num - 1]
+    }
 };
 
 // Do not edit below this line
