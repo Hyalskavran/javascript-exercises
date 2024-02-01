e53ab37be5787b87cf80ddeaa2202a76bd0f8dc8
Hyalskavran <gorniakadrian94@gmail.com>
2024-02-01 12:23:19 +0100

    exercise done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let oldest = []
+    const currentYear = new Date().getFullYear()
 
+    arr.forEach(person => {
+        let death = 0
+
+        if (Object.keys(person).includes('yearOfDeath')) {
+            death = person['yearOfDeath']
+        } else {
+            death = currentYear
+        }
+
+        let yearsLived = death - person['yearOfBirth']
+        
+        if (oldest.length === 0) {
+            oldest.push([person, yearsLived])
+        } else {
+            if (oldest[0][1] < yearsLived) {
+                oldest.pop()
+                oldest.push([person, yearsLived])
+            }
+        }
+        }
+    );
+
+    return oldest[0][0]
 };
 
 // Do not edit below this line
