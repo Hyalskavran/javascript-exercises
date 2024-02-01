3fa17e8bd6dc05b4d494375a0d5bb8976314ec2c
Hyalskavran <gorniakadrian94@gmail.com>
2024-02-01 11:53:12 +0100

    exercise done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function(objArr) {
+    arr = []
 
+    objArr.forEach(obj => {
+        for ([k, v] of Object.entries(obj)) {
+            if (k == 'title') {
+                arr.push(v)
+            }
+        }
+    });
+
+    return arr
 };
 
 // Do not edit below this line
